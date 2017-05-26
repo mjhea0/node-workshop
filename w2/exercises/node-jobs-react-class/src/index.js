39d31575ef4cdc00075914bdc4ba20b0951b2fb0
Michael Herman <hermanmu@gmail.com>
2017-05-26 14:07:02 -0600

    updated slides and added example

---
+++ w2/exercises/node-jobs-react-class/src/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+import JobList from './components/JobList';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      jobs: []
+    }
+  }
+  componentDidMount() {
+    this.getJobs();
+  }
+  getJobs() {
+    axios.get(`http://localhost:8080/api/v1/jobs`)
+    .then((res) => { this.setState({ jobs: res.data.data }); })
+    .catch((err) => { console.log(err); })
+  }
+  render() {
+    return (
+      <div className="container">
+        <h1>Node Jobs</h1>
+        <JobList jobs={ this.state.jobs }/>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
